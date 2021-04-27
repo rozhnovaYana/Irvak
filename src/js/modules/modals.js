6166a6658a7b341d4ad74d4370f1dddf426ac841
Yana <scream.yv@gmail.com>
2021-04-27 16:59:19 +0300

    modalsHandler was created

---
+++ src/js/modules/modals.js
@@ -0,0 +1,38 @@
+const modals = () => {
+    const modalHandler = (modalSelector, closeSelector, targetSelector) => {
+        const modal = document.querySelector(modalSelector),
+            close = modal.querySelector(closeSelector),
+            target = document.querySelectorAll(targetSelector);
+        target.forEach(item => {
+            item.addEventListener("click", (e) => {
+                if (e.target) {
+                    e.preventDefault()
+                }
+                modal.style.display = "block"
+                document.body.style.overflow = "hidden"
+                
+            })
+
+        })
+        close.addEventListener("click", ()=> {
+                modal.style.display = "none"
+                document.body.style.overflow = ""
+        })
+        modal.addEventListener("click", (e) => {
+            if (e.target === modal) {
+                modal.style.display = "none"
+                document.body.style.overflow = ""
+            }
+        })
+    }
+    const showModal = (modalSelector) => {
+        document.querySelector(modalSelector).style.display = "block"
+        document.body.style.overflow = ""
+    }
+    modalHandler(".popup_engineer", '.popup_close', ".popup_engineer_btn")
+    modalHandler(".popup", '.popup_close', ".phone_link")
+    setTimeout(() => {
+        showModal(".popup")
+    }, 3000)
+}
+export default modals
