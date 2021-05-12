4920c224429fd25a6b6c63b462437643daf0dec0
Yana <scream.yv@gmail.com>
2021-05-12 15:53:44 +0300

    final build prod

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -3,7 +3,9 @@ const modals = () => {
         const modal = document.querySelector(modalSelector),
             close = modal.querySelector(closeSelector),
             target = document.querySelectorAll(targetSelector),
-            windows=document.querySelectorAll("[data-modal]")
+            windows=document.querySelectorAll("[data-modal]"),
+            scroll=scroolWidth()
+            
         target.forEach(item => {
             item.addEventListener("click", (e) => {
                 if (e.target) {
@@ -12,6 +14,7 @@ const modals = () => {
                 windows.forEach(i=>i.style.display="none")
                 modal.style.display = "block"
                 document.body.style.overflow = "hidden"
+                document.body.style.marginRight=`${scroll}px`
                 
             })
 
@@ -20,26 +23,33 @@ const modals = () => {
             windows.forEach(i=>i.style.display="none")
                 modal.style.display = "none"
                 document.body.style.overflow = ""
+                document.body.style.marginRight=`0px`
         })
         modal.addEventListener("click", (e) => {
             if (e.target === modal&&closeClickOverlay) {
                 windows.forEach(i=>i.style.display="none")
                 modal.style.display = "none"
                 document.body.style.overflow = ""
+                document.body.style.marginRight=`0px`
             }
         })
     }
-    const showModal = (modalSelector) => {
-        document.querySelector(modalSelector).style.display = "block"
-        document.body.style.overflow = ""
+
+    const scroolWidth=()=>{
+        let div = document.createElement('div');
+        div.style.overflowY = 'scroll';
+        div.style.width = '50px';
+        div.style.height = '50px';
+        document.body.append(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+        return scrollWidth
     }
     modalHandler(".popup_engineer", '.popup_close', ".popup_engineer_btn")
     modalHandler(".popup", '.popup_close', ".phone_link")
     modalHandler(".popup_calc", ".popup_calc_close", ".popup_calc_btn")
     modalHandler(".popup_calc_profile", ".popup_calc_profile_close",".popup_calc_button", false)
     modalHandler(".popup_calc_end", ".popup_calc_end_close",".popup_calc_profile_button", false)
-    // setTimeout(() => {
-    //     showModal(".popup")
-    // }, 6000)
+
 }
 export default modals
